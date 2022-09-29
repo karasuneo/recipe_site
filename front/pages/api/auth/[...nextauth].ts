22e2c546873091e4a20210b1b4e95b1df5debafe
Emma Paris <yusei4520030401@gmail.com>
2022-09-29 10:57:48 +0900

    セッションとCokkieの実装

--- front/pages/api/auth/[...nextauth].ts
+++ front/pages/api/auth/[...nextauth].ts
@@ -9,9 +9,13 @@ export default NextAuth({
     }),
   ],
   callbacks: {
-    async signIn({ user, account, profile, email, credentials }) {
-      console.log('サインイン');
-      return true;
+    async session({ session, user, token }) {
+      session.accessToken = token.accessToken
+      return session;
+    },
+    async jwt({ token, user, account, profile, isNewUser }) {
+      if (account) token.accessToken = account.access_token;
+      return token;
     },
   },
 });
--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -3,6 +3,10 @@ import { useSession, signIn, signOut } from "next-auth/react"
 export default function Component() {
   const { data: session } = useSession()
   if (session) {
+    console.log(session);
+  }
+  if (session) {
+    
     return (
       <>
         Signed in as {session.user.name} <br />

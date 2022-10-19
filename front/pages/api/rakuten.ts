f1cfe74ff4e86c31d643f4f40b148d068d53dcdf
Emma Paris <yusei4520030401@gmail.com>
2022-10-19 12:47:50 +0900

    メモに追加

--- front/pages/api/rakuten.ts
+++
@@ -1,7 +0,0 @@
-export default async function handler(req, res) {
-  const response = await fetch(
-    "https://app.rakuten.co.jp/services/api/Recipe/CategoryList/20170426?applicationId=1039167989114446384"
-  );
-  const rakutens = await response.json();
-  res.status(200).json({ rakutens });
-}
